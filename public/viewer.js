f8b080bfd94044c2b4a1f23533fe8cfb828689d3
Mai Van Manh <mvmanh@gmail.com>
2023-10-29 10:11:42 +0700

    add TURN server

--- public/viewer.js
+++ public/viewer.js
@@ -11,11 +11,7 @@ async function init() {
 
 function createPeer() {
     const peer = new RTCPeerConnection({
-        iceServers: [
-            {
-                urls: "stun:stun.stunprotocol.org"
-            }
-        ]
+        iceServers: [{   urls: [ "stun:ss-turn1.xirsys.com" ]}, {   username: "84B9tUvDm5vHM780N2N7bB723gnWaoA9mgJ0QIH5Fg5x72wfeZaDHKtXAdY8GhTxAAAAAGU9viptdm1hbmg=",   credential: "c91a3e0a-75ff-11ee-8961-0242ac140004",   urls: [       "turn:ss-turn1.xirsys.com:80?transport=udp",       "turn:ss-turn1.xirsys.com:3478?transport=udp",       "turn:ss-turn1.xirsys.com:80?transport=tcp",       "turn:ss-turn1.xirsys.com:3478?transport=tcp",       "turns:ss-turn1.xirsys.com:443?transport=tcp",       "turns:ss-turn1.xirsys.com:5349?transport=tcp"   ]}]
     });
     peer.ontrack = handleTrackEvent;
     peer.onnegotiationneeded = () => handleNegotiationNeededEvent(peer);
